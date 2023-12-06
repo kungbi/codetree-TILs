07a6137c6eaca40739b92e6db668221e6d9bb28f
Shin Woongbi <kipper12335@gmail.com>
2023-12-06 21:22:57 +0900

    Title: 숫자 합치기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231206/숫자 합치기/merge-numbers.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+cost = 0
+
+def f(input_list):
+    n = len(input_list)
+    tmp_list = []
+    cost = 0
+    
+    for i in range(0, n - 1, 2):
+        tmp_list.append(input_list[i] + input_list[i+1])
+        cost += input_list[i] + input_list[i+1]
+    if len(tmp_list) != 1:
+        return cost + f(tmp_list)
+
+    return cost
+
+result = f(arr)
+print(result)
