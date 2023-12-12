a18415b0967b05909183950e264976c64c05cc4a
Shin Woongbi <kipper12335@gmail.com>
2023-12-12 14:10:15 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 157ms, Memory: 31MB, Status: Passed - CodeTree

--- 231212/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 231212/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -2,23 +2,23 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a_arr = []
-for _ in range(n):
-    a_arr.append(int(input()))
-
 b_arr = []
-for num in range(1, n * 2 + 1):
-    if num not in a_arr:
-        b_arr.append(num)
+for _ in range(n):
+    b_arr.append(int(input()))
+b_set = set(b_arr)
 
-score = 0
+a_arr = [
+    num for num in range(1, 2 * n + 1) if num not in b_set
+]
+
 a_arr.sort()
 b_arr.sort()
-ai = bi = 0
-while ai < n and bi < n:
-    if a_arr[ai] < b_arr[bi]:
-        ai += 1
+score = 0
+a_idx = b_idx = 0
+while a_idx < n and b_idx < n:
+    if a_arr[a_idx] > b_arr[b_idx]:
+        b_idx += 1
         score += 1
-    bi += 1
+    a_idx += 1
 
 print(score)
