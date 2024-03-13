c8a55f1f037fc45fefe7c3388c3d12dd1e51aa03
Woongbi Shin <dobby12335@gmail.com>
2024-03-13 14:40:42 +0900

    Title: 수 채우기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/수 채우기/fill-in-number.py
+++ 240313/수 채우기/fill-in-number.py
@@ -1,17 +1,13 @@
 n = int(input())
 
-result = 0
-while n != 0:
-    if 6 == n:
-        n -= 6
-        result += 3
-        break
-    elif 5 <= n:
-        n -= 5
-    elif 2 <= n:
-        n -= 2
-    else:
-        result = -1
-        break
-    result += 1
-print(result)
+
+result = n // 5
+n = n % 5
+
+result += n // 2
+n = n % 2
+
+if n == 0:
+    print(result)
+else:
+    print(-1)
