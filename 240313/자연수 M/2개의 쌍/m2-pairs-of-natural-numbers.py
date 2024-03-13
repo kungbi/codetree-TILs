4a4eb08e8fbb6acba4f9160134daf02c3511c18d
Woongbi Shin <dobby12335@gmail.com>
2024-03-13 21:11:10 +0900

    Title: 자연수 M/2개의 쌍, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240313/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -12,16 +12,27 @@ def solution():
     heap = []
     for _ in range(n):
         x, y = map(int, input().split())
-        for _ in range(x):
-            heappush(heap, -y)
+        heappush(heap, (-y, x))
     
     result = float('inf')
     while 2 <= len(heap):
-        a = -heappop(heap)
-        b = -heappop(heap)
-        c = a * b
-        result = min(result, c)
+        a_y, a_x= heappop(heap)
+        while heap and a_x % 2 == 1:
+            a_y, a_x= heappop(heap)
+        a_y *= -1
 
+        if not heap:
+            print(a_y)
+            return
+
+        b_y, b_x= heappop(heap)
+        while heap and b_x % 2 == 1:
+            b_y, b_x= heappop(heap)
+        b_y *= -1
+
+        result = min(result, a_y * b_y)
+
+        
     print(result)
 
 
